fb5ee2bcf921cd4c729c4e3d113607031619b6c5
jawher00101~ <jawherj75@gmail.com>
2023-01-30 00:59:37 +0100

    gg

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,13 +1,20 @@
 import { Params, useParams } from "react-router-dom";
 
-const MovieInfo=(Movie)=> {
-  const {idmovie}=useParams()
-  const moviein =Movie.find((el)=> el.id == idmovie )
-    return (
-      <div>
-   <img src={moviein.posterURL} />
-   </div>
-    );
-  }
-  
-  export default MovieInfo;
+const MovieInfo = (Movie) => {
+  const { idmovie } = useParams();
+  const movie = Movie.find((el) => el.id == idmovie);
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      <img src={movie.posterURL} />
+      <p>{movie.description}</p>
+      <iframe
+        height="100%"
+        src={movie.trailer}
+        title="Trailer"
+      ></iframe>
+    </div>
+  );
+};
+
+export default MovieInfo;
